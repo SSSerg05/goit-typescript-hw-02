e63d5dcc1d13ad8d73328d30fddbf07f1a3c779b
SSSerg05 <ssserg05@gmail.com>
2023-10-31 22:05:34 +0200

    fix 5g
    Signed-off-by: SSSerg05 <ssserg05@gmail.com>

--- src/generic/5.ts
+++ src/generic/5.ts
@@ -3,7 +3,7 @@
   Використовуйте generics, щоб цей інтерфейс міг працювати з будь-якими типами ключів та значень.
 */
 
-interface KeyValuePair<T,U> {
+interface KeyValuePair<T extends string | symbol, U extends any> {
   key: T;
   value: U;
 }
